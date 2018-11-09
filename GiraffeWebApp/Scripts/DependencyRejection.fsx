2b65dd3124017927e5c27a7d3be809246cc5f83d
Brett Rowberry <brett@Bretts-MacBook-Pro.local>
2018-11-08 20:36:47 -0600

    Added DependencyRejection

---
+++ GiraffeWebApp/Scripts/DependencyRejection.fsx
@@ -0,0 +1,6 @@
+//http://blog.ploeh.dk/2017/02/02/dependency-rejection/
+//Partial application not a good way to do dependency injection?
+//Eirik Tsarpalis //Currying for dependency injection should be considered harmful. ... use reader monad
+// PROBLEM: what's configuration what's for the caller to supply???
+
+//composition root
